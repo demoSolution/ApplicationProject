6ba87d49fb770f08884d3639b690136c14b81cc7
AkshayMobileApplicationDeveloper <akshaykumarprajapati@hotmail.com>
2024-08-21 02:04:53 +0530

    master

---
+++ GroceryApp/app/src/main/java/demo/solution/groceryapp/Log/ForgetPasswordActivity.java
@@ -0,0 +1,18 @@
+package demo.solution.groceryapp.Log;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import demo.solution.groceryapp.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_forget_password);
+
+    }
+}
